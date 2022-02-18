cb86713e9b6bdb1b4cd216cc8509a715aba2f321
SPT <arthit0ak@gmail.com>
2022-02-18 20:20:14 +0700

    add template

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const debug = require('debug')
 const morgan = require('morgan')
 const chalk = require('chalk')
 const port =3000;
+const path = require('path');
+
+
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'/public/')))
 
 app.get('/',(req,res)=>{
     res.send('hello')
 
 })
 app.listen(port,()=>{
-  debug('listening on port:'+ chalk.green(port));
+  console.log('listening on port:'+ chalk.green(port));
 })
